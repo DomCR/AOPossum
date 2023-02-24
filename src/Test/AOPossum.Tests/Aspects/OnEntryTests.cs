e4403465ac280f44c4c2a0a86557a75ac11e399e
DomCR <DomCR@users.noreply.github.com>
2023-02-24 13:10:30 +0100

    params

--- src/Test/AOPossum.Tests/Aspects/OnEntryTests.cs
+++ src/Test/AOPossum.Tests/Aspects/OnEntryTests.cs
@@ -37,5 +37,55 @@ namespace AOPossum.Tests.Aspects
 			Assert.NotNull(args);
 			Assert.Empty(args.Parameters);
 		}
+
+		[Fact]
+		public void OnEntryWithParamater()
+		{
+			TypeDefinition t = _mock.MainModule.GetType(typeof(Mock.Shared.Logging.MockConsoleLogMethodLevel).FullName);
+			MethodDefinition m = t.Methods.FirstOrDefault(m => m.Name == nameof(Mock.Shared.Logging.MockConsoleLogMethodLevel.MethodToLogWithParam));
+
+			m.AddOnEntryAspectWithParams(typeof(OnMethodBoundaryMock));
+
+			Assembly mock = reloadMockAssembly();
+
+			string param = "hellow this is a parameter";
+			dynamic instance = mock.CreateInstance(typeof(Mock.Shared.Logging.MockConsoleLogMethodLevel).FullName);
+			instance.MethodToLogWithParam(param);
+
+			Type boundary = mock.DefinedTypes.FirstOrDefault(o => o.FullName == typeof(OnMethodBoundaryMock).FullName);
+			object hasEnteredValue = boundary.GetField(nameof(OnMethodBoundaryMock.HasExecutedOnEntry)).GetValue(null);
+			Assert.True((bool)hasEnteredValue);
+
+			MethodExecutionArgs args = (MethodExecutionArgs)boundary.GetField(nameof(OnMethodBoundaryMock.OnEntryArgs)).GetValue(null);
+			Assert.NotNull(args);
+			Assert.NotEmpty(args.Parameters);
+			Assert.Equal(param, args.Parameters.FirstOrDefault());
+		}
+
+		[Fact]
+		public void OnEntryWithMultipleParamaters()
+		{
+			TypeDefinition t = _mock.MainModule.GetType(typeof(Mock.Shared.Logging.MockConsoleLogMethodLevel).FullName);
+			MethodDefinition m = t.Methods.FirstOrDefault(m => m.Name == nameof(Mock.Shared.Logging.MockConsoleLogMethodLevel.MethodToLogWithMultipleParam));
+
+			m.AddOnEntryAspectWithParams(typeof(OnMethodBoundaryMock));
+
+			Assembly mock = reloadMockAssembly();
+
+			string param1 = "parameter 1";
+			string param2 = "parameter 2";
+			dynamic instance = mock.CreateInstance(typeof(Mock.Shared.Logging.MockConsoleLogMethodLevel).FullName);
+			instance.MethodToLogWithMultipleParam(param1, param2);
+
+			Type boundary = mock.DefinedTypes.FirstOrDefault(o => o.FullName == typeof(OnMethodBoundaryMock).FullName);
+			object hasEnteredValue = boundary.GetField(nameof(OnMethodBoundaryMock.HasExecutedOnEntry)).GetValue(null);
+			Assert.True((bool)hasEnteredValue);
+
+			MethodExecutionArgs args = (MethodExecutionArgs)boundary.GetField(nameof(OnMethodBoundaryMock.OnEntryArgs)).GetValue(null);
+			Assert.NotNull(args);
+			Assert.NotEmpty(args.Parameters);
+			Assert.Equal(param1, args.Parameters.ElementAt(0));
+			Assert.Equal(param2, args.Parameters.ElementAt(1));
+		}
 	}
 }
